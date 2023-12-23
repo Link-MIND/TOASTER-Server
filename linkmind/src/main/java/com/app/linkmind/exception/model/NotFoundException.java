c90deb9382a76dbf3622ca3a970fb25ede49b651
soohyun <sss4920@naver.com>
2023-12-23 15:03:34 +0900

    fix: 초기세팅 수정

--- linkmind/src/main/java/com/app/linkmind/exception/model/NotFoundException.java
+++ linkmind/src/main/java/com/app/linkmind/exception/model/NotFoundException.java
@@ -1,5 +1,7 @@
 package com.app.linkmind.exception.model;
 
+import com.app.linkmind.exception.Error;
+
 public class NotFoundException extends CustomException {
 	public NotFoundException(Error error, String message) {
 		super(error, message);
