9e9e0f6fde75680304d2ed07df3d2fa7f7c885cd
mmihye <smilehye910@naver.com>
2023-12-31 23:48:24 +0900

    [#11] refactor : 토큰 시간 변경

--- linkmind/src/main/java/com/app/toaster/service/auth/AuthService.java
+++ linkmind/src/main/java/com/app/toaster/service/auth/AuthService.java
@@ -28,8 +28,9 @@ public class AuthService {
 	private final UserRepository userRepository;
 
 
-	private final Long TOKEN_EXPIRATION_TIME_ACCESS = 100 * 24 * 60 * 60 * 1000L;
-	private final Long TOKEN_EXPIRATION_TIME_REFRESH = 200 * 24 * 60 * 60 * 1000L;
+	private final Long TOKEN_EXPIRATION_TIME_ACCESS = 1 * 60 * 1000L;
+	private final Long TOKEN_EXPIRATION_TIME_REFRESH = 3 * 60 * 1000L;
+
 
 	@Transactional
 	public SignInResponseDto signIn(String socialAccessToken, SignInRequestDto requestDto) {
