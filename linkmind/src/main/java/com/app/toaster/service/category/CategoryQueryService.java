e3f1ca9423fad87a4bd42bd34afbd915f6251036
soohyun <sss4920@naver.com>
2024-01-11 23:51:58 +0900

    [#43] refactor: CategoryQueryService 등 깡통 service 추가

---
+++ linkmind/src/main/java/com/app/toaster/service/category/CategoryQueryService.java
@@ -0,0 +1,12 @@
+package com.app.toaster.service.category;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class CategoryQueryService {
+
+	// R
+}
