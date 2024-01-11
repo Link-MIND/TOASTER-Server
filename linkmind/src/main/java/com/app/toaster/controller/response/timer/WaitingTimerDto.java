531967a7b323e07c1b1e6241cf1ac2f19574868c
mmihye <smilehye910@naver.com>
2024-01-12 07:00:02 +0900

    [#48] feature : 대기중인 타이머 response에 카테고리 아이디 추가

--- linkmind/src/main/java/com/app/toaster/controller/response/timer/WaitingTimerDto.java
+++ linkmind/src/main/java/com/app/toaster/controller/response/timer/WaitingTimerDto.java
@@ -6,8 +6,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public record WaitingTimerDto(Long timerId, String remindTime, String remindDates, Boolean isAlarm, LocalDateTime updateAt, String comment) {
+public record WaitingTimerDto(Long timerId, String remindTime, String remindDates, Boolean isAlarm, LocalDateTime updateAt, String comment, Long categoryId) {
     public static WaitingTimerDto of(Reminder timer, String remindTime, String remindDates) {
-        return new WaitingTimerDto(timer.getId(), remindTime, remindDates, timer.getIsAlarm(), timer.getUpdateAt(), timer.getComment());
+        return new WaitingTimerDto(timer.getId(), remindTime, remindDates, timer.getIsAlarm(), timer.getUpdateAt(), timer.getComment(), timer.getCategory().getCategoryId());
     }
 }
