3110f2eb4221d5829d8d4cc0875e3b41d170892c
mmihye <smilehye910@naver.com>
2024-01-12 06:53:51 +0900

    [#48] feature : 대기중인 타이머 response에 comment 추가

--- linkmind/src/main/java/com/app/toaster/controller/response/timer/WaitingTimerDto.java
+++ linkmind/src/main/java/com/app/toaster/controller/response/timer/WaitingTimerDto.java
@@ -6,8 +6,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public record WaitingTimerDto(Long timerId, String remindTime, String remindDates, Boolean isAlarm, LocalDateTime updateAt) {
+public record WaitingTimerDto(Long timerId, String remindTime, String remindDates, Boolean isAlarm, LocalDateTime updateAt, String comment) {
     public static WaitingTimerDto of(Reminder timer, String remindTime, String remindDates) {
-        return new WaitingTimerDto(timer.getId(), remindTime, remindDates, timer.getIsAlarm(), timer.getUpdateAt());
+        return new WaitingTimerDto(timer.getId(), remindTime, remindDates, timer.getIsAlarm(), timer.getUpdateAt(), timer.getComment());
     }
 }
