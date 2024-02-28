dddf77012301ca922d43e359ba7d30f9725d45aa
soohyun <sss4920@naver.com>
2024-02-28 16:51:03 +0900

    [#229] refactor: 테스트 코드 수정 및 프로토콜이 맞지않을 때 테스트 케이스 추가

--- linkmind/src/test/java/com/app/toaster/toast/ToastRepositoryTest.java
+++ linkmind/src/test/java/com/app/toaster/toast/ToastRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.app.toaster.base.BaseRepositoryTest;
 import com.app.toaster.domain.Toast;
 import com.app.toaster.domain.User;
 import com.app.toaster.exception.Error;
@@ -14,10 +15,7 @@ import com.app.toaster.exception.model.NotFoundException;
 import com.app.toaster.infrastructure.ToastRepository;
 import com.app.toaster.infrastructure.UserRepository;
 
-@DataJpaTest //@Transactional 어노테이션을 포함하고 있다. 그래서 테스트가 완료되면 자동으로 롤백.
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("local")
-public class ToastRepositoryTest {
+public class ToastRepositoryTest extends BaseRepositoryTest {
 
 	@Autowired
 	ToastRepository toastRepository;
@@ -26,7 +24,7 @@ public class ToastRepositoryTest {
 	UserRepository userRepository;
 
 	@Test
-	public void 카테고리가null일때_저장_테스트(){
+	public void 카테고리가_null일때_저장_테스트(){
 		//given
 		String url ="https://www.naver.com";
 
@@ -49,5 +47,33 @@ public class ToastRepositoryTest {
 		System.out.println(toast.getId());
 		Assertions.assertEquals(toast.getTitle(),"하이");
 		Assertions.assertEquals(toast.getThumbnailUrl(),"기본이미지url");
+	}
+
+	@Test
+	public void url의프로토콜이_잘못된_경우_테스트(){
+		//given
+		String url ="www.naver.com";
+
+		User presentUser =  userRepository.findByUserId(125L).orElseThrow(
+			()-> new NotFoundException(Error.NOT_FOUND_USER_EXCEPTION, Error.NOT_FOUND_USER_EXCEPTION.getMessage())
+		);
+
+		//when
+		Toast toast = Toast.builder()
+			.user(presentUser)
+			.linkUrl(url)
+			.title("하이")
+			.thumbnailUrl("기본이미지url")
+			.build();
+		// 카테고리 null로 테스트
+		toast.updateCategory(null);
+		toastRepository.save(toast);
+
+		//then
+		System.out.println(toast.getId());
+		System.out.println(toast.getLinkUrl().equals("www.naver.com"));
+		Assertions.assertEquals(toast.getTitle(),"하이");
+		Assertions.assertEquals(toast.getThumbnailUrl(),"기본이미지url");
+
 	}
 }
