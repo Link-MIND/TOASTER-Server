78b036d4e8c5bf7776b7f289f494837e69f54fac
soohyun <sss4920@naver.com>
2024-09-16 17:35:02 +0900

    [#240] feature: user 회원탈퇴 시 manager도 비우도록 설정

--- linkmind/src/main/java/com/app/toaster/popup/infrastructure/PopupManagerRepository.java
+++ linkmind/src/main/java/com/app/toaster/popup/infrastructure/PopupManagerRepository.java
@@ -11,4 +11,6 @@ import com.app.toaster.popup.entity.PopupInvisibleManager;
 public interface PopupManagerRepository extends JpaRepository<PopupInvisibleManager, Long> {
 	List<PopupInvisibleManager> findByUserId(Long userId);
 	Optional<PopupInvisibleManager> findByUserIdAndPopup(Long userId, Popup popup);
+
+	void deleteAllByUserId(Long userId);
 }
