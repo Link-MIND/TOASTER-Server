898fa9d08710c05751c4af3c5f109edc3a4ee309
mmihye <smilehye910@naver.com>
2024-10-24 20:31:37 +0900

    [#243] fix: 필요없는 import 삭제

--- linkmind/src/main/java/com/app/toaster/controller/HealthCheckController.java
+++ linkmind/src/main/java/com/app/toaster/controller/HealthCheckController.java
@@ -4,11 +4,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.toaster.controller.request.toast.IsReadDto;
-import com.app.toaster.controller.response.toast.IsReadResponse;
-import com.app.toaster.domain.User;
-import com.app.toaster.exception.Error;
-import com.app.toaster.exception.model.NotFoundException;
 import com.app.toaster.external.client.discord.DiscordMessageProvider;
 import com.app.toaster.external.client.discord.NotificationDto;
 import com.app.toaster.external.client.discord.NotificationType;
